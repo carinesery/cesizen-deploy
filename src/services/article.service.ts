00c911d6a52abe8865b17522accc747505c6cf2c
Carine Sery <carine.sery@gmail.com>
2026-01-22 16:00:04 +0100

    Création d'un article sans media

--- src/services/article.service.ts
+++ src/services/article.service.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../prismaClient.js";
+import { generateSlug } from "../utils/generatedSlug.js";
 
 export const getPublicArticles = async () => {
     return prisma.article.findMany({
@@ -8,7 +9,7 @@ export const getPublicArticles = async () => {
             slug: true,
             summary: true,
             createdAt: true,
-            updatedAt: true, 
+            updatedAt: true,
             categories: {
                 select: {
                     title: true,
@@ -21,6 +22,38 @@ export const getPublicArticles = async () => {
         },
         orderBy: {
             updatedAt: "desc"
+        },
+    });
+};
+
+type CreateArticleInput = {
+    title: string;
+    content?: string;
+    summary?: string;
+    presentationImageUrl?: string;
+    authorId: number; // devra venir de auth donc à modifier ensuite
+    status?: "DRAFT" | "PUBLISHED";
+}
+export const createArticle = async (data: CreateArticleInput) => {
+    const slug = generateSlug(data.title);
+
+    const existingSlug = await prisma.article.findUnique({
+        where: { slug },
+    });
+
+    if (existingSlug) {
+        throw new Error("ARTICLE_SLUG_ALREADY_EXISTS");
+    };
+
+    return prisma.article.create({
+        data: {
+            title: data.title,
+            slug,
+            content: data.content,
+            summary: data.summary,
+            presentationImageUrl: data.presentationImageUrl,
+            authorId: data.authorId,
+            status: data.status,
         },
     });
 };
