db357d6bf85a1ee907d993a99d366210fbfbfc6d
Carine Sery <carine.sery@gmail.com>
2026-03-08 15:42:40 +0100

    Ajout de la lecture d'un profil par un admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validate.middleware.js";
 import { adminRegisterSchema } from "../schemas/admin.schema.js";
-import { getAllUsersController, adminCreateUserController } from "../controllers/admin.controller.js";
+import { getAllUsersController, getUserController, adminCreateUserController } from "../controllers/admin.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { roleMiddleware } from "../middlewares/role.middleware.js";
 import { UserRoleEnum } from "../utils/enum.js";
@@ -10,6 +10,7 @@ import { UserRoleEnum } from "../utils/enum.js";
 const router = Router();
 
 router.get("/users", authMiddleware, roleMiddleware(UserRoleEnum.ADMIN), getAllUsersController);
+router.get("/user/:idUser", authMiddleware, roleMiddleware(UserRoleEnum.ADMIN), getUserController);
 router.post("/user", authMiddleware, roleMiddleware(UserRoleEnum.ADMIN), validate(adminRegisterSchema, "body"), adminCreateUserController);
 // router.get("/test", (req, res) => {
 //   res.send("ADMIN ROUTE OK");
