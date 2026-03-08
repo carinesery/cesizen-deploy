ac1f005e7afafc27935983cecd237be2002c9200
Carine Sery <carine.sery@gmail.com>
2026-03-08 14:25:47 +0100

    Ajout de la liste de tous les utilisateurs pour l'admin

---
+++ src/services/admin.service.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../prismaClient.js";
+
+export const getAllUsersService = async() => {
+
+    const users = await prisma.user.findMany(
+        { select: {
+            idUser: true,
+            username: true,
+            email: true,
+            profilPictureUrl: true,
+            role: true,
+            isActive: true,
+            createdAt: true,
+            updatedAt: true,
+            // deletedAt: true,
+        }}
+    )
+
+     return users;
+}
