cf229f5704f21cef8a286f61fc23dcc2ecd9ceb0
Carine Sery <carine.sery@gmail.com>
2026-03-09 14:27:14 +0100

    Ajout du password confirm dans Zod

--- src/schemas/admin.schema.ts
+++ src/schemas/admin.schema.ts
@@ -18,7 +18,11 @@ export const adminRegisterSchema = z.object({
         .min(8, "Le mot de passe doit contenir au moins 8 caractères")
         .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9\s]).{8,50}$/, "Le mot de passe est invalide")
         .max(50, "Le mot de passe ne doit pas dépasser 50 caractères"),
+    confirmPassword: z.string(),
     role: z.enum(UserRoleEnum)
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Les mots de passe ne correspondent pas",
+    path: ["confirmPassword"],
 });
 
 export const adminUpdateUserSchema = z.object({
