6da6077569cb59ad45355aa8ec8f77530106519a
Carine Sery <carine.sery@gmail.com>
2026-06-17 02:05:08 +0200

    Fix : running migrations

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "🚀 Running database migrations..."
 
-npx prisma migrate deploy --schema=src/prisma/schema.prisma
+npx prisma migrate deploy --config=src/prisma.config.ts
 
 echo "✅ Migrations done"
 
