dc42756117c30b78a894a2f4ae78e0ccc3c41163
Carine Sery <carine.sery@gmail.com>
2026-06-18 15:53:30 +0200

    Feature : Add test login

---
+++ frontend/cypress/e2e/login.cy.ts
@@ -0,0 +1,22 @@
+describe('Login', () => {
+  it('connecte un administrateur', () => {
+    cy.visit('/login');
+
+    cy.get('input[type="email"]').type(
+      Cypress.env('ADMIN_EMAIL')
+    );
+
+    cy.get('input[type="password"]').type(
+      Cypress.env('ADMIN_PASSWORD'),
+      { log: false }
+    );
+
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('not.include', '/login');
+
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('token')).to.not.be.null;
+    });
+  });
+});
