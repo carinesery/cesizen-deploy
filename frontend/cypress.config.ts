8092effa28e5dc0afe437bf6c3de3b94c2b9e871
Carine Sery <carine.sery@gmail.com>
2026-06-18 16:16:21 +0200

    Fix : tests e2e video

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://20.233.249.136:5173',  // ← Juste pour CI local
+    baseUrl: 'http://20.233.249.136:5173', 
+    video: true, // IMPORTANT
+     // ← Juste pour CI local
     setupNodeEvents(on, config) {
       // Pas besoin de rien ici
     },
